ddea3b3302cfa88e98401ce1dcf7f467f6d72e15
Michel Lang <michellang@gmail.com>
2019-09-20 00:56:01 +0200

    use '<-' for assignment

--- R/binary_classification.R
+++ R/binary_classification.R
@@ -58,7 +58,7 @@ auc <- function(actual, predicted) {
 #' predicted <- c(0.9, 0.8, 0.4, 0.5, 0.3, 0.2)
 #' ll(actual, predicted)
 ll <- function(actual, predicted, eps = 1e-15) {
-    predicted = pmax(eps, pmin(1 - eps, predicted))
+    predicted <- pmax(eps, pmin(1 - eps, predicted))
     return(-ifelse(actual, log(predicted), log(1 - predicted)))
 }
 
